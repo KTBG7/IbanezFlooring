4c6e2cc68bcb951f8968d963ea6221f3b67dd71e
Kevin Ibanez <kpeibanez@gmail.com>
2022-03-22 19:24:30 -0500

    Ibanez-2: Added router with switch and route to home page

--- ibanez-flooring-app/src/App.js
+++ ibanez-flooring-app/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Home from './pages/Home'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router> 
   );
 }
 
